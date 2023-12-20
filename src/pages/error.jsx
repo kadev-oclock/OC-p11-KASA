8263721471e68aa70e373086e8d919dca229fee5
kadev <karimaoudiaa@gmail.com>
2023-12-20 15:26:56 +0100

    add error-page

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import "./error.scss";
+import { Link } from "react-router-dom";
 
 function Error() {
-  return <div>error</div>;
+  return <>
+   <main className="error">
+  <h1 className="error__name">404</h1>
+  <h2 className="error__intro">Oups! La page que vous demandez n'existe pas.</h2>
+  <Link className="error__link"  to="/">Retourner sur la page d’accueil</Link>
+   </main>
+    
+  </>;
 }
 export default Error;
